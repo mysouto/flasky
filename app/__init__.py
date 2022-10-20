fe0e9c2803794eb80682a6c557e479986f083392
Milena Souto <yamadasouto@gmail.com>
2022-10-20 10:33:50 -0700

    add comments to init file create app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+# don't need to memorize, know how to write
+# can refer back from previous projects
 
+
+# function needs to be named exactly "create_app"
+# when `run Flask` it will look for create_app function
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
+    # creates app instance and returns it
     return app
