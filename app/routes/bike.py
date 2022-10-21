7934f4e2f89faa964ebabcfb06aca39f8cb8ed9b
Milena Souto <yamadasouto@gmail.com>
2022-10-21 10:58:22 -0700

    Add a new route to bike blueprint

--- app/routes/bike.py
+++ app/routes/bike.py
@@ -1,4 +1,3 @@
-import re
 from flask import Blueprint, jsonify
 
 
@@ -30,8 +29,7 @@ bikes = [
 bike_bp = Blueprint("bike_bp", __name__, url_prefix="/bike")
 
 
-# DEFINING AN ENDPOINT
-# 4. create function to return JSON for client
+# 4. create route function to return JSON for client
 # 5. add decorator with arguments (args define what type of request will be routed to this function)
 # "" that follows /bike, methods=[requests]
 @bike_bp.route("", methods=["GET"])
@@ -60,3 +58,35 @@ def get_all_bikes():
     return jsonify(response), 200
 
 # 6. Make sure Flask knows app functions exits -> go to init file
+
+
+# CREATING A NEW ROUTE
+@bike_bp.route("/<bike_id>", methods=["GET"])
+def get_one_bike(bike_id):
+    # 1. valide bike id input
+    # see if bike_id can be converted to an int
+    # try-except: try to convert to int, if error: catch and raise 400 error
+    try:
+        bike_id = int(bike_id)
+    except ValueError:
+        response_str = f"Invalid bike_id: {bike_id}. ID must be a integer."
+        return jsonify({"message": response_str}), 400
+
+    # after try-except: bike_id will be a valid int
+    # 2. iterate thru data to find item with matching id
+    for bike in bikes:
+        if bike.id == bike_id:
+            # if id matched, create dict
+            bike_dict = {
+                "id": bike.id,
+                "name": bike.name,
+                "price": bike.price,
+                "size": bike.size,
+                "type": bike.type,
+            }
+            # 3. if id in data, return bike's data with 200 response 200
+            return jsonify(bike_dict), 200
+
+    # 4. after loop: bike with matching id not found -> raise "404 object not found" error
+    response_message = f"Could not find bike with ID {bike_id}"
+    return jsonify({"message": response_message}), 404
