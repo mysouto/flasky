dd1c079bde6f39a01ad8c9c4531578043d187404
Milena Souto <yamadasouto@gmail.com>
2022-10-27 11:25:34 -0700

    Set up postgres bikes_development database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 # don't need to memorize, know how to write
 # can refer back from previous projects
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 # APP FUNCTION registers blueprint
@@ -9,6 +14,17 @@ from flask import Flask
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+
+    # CONFIG FOR DATABASE
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # where database is on the internet
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/bikes_developement"
+
+    # CONNECT DATABASE TO APP
+    # flask app/server
+    db.init_app(app)
+    # migrate help set up database
+    migrate.init_app(app, db)
 
     # 6. final steps: Make sure Flask knows app functions exits -> go to init file (from bike file)
     # -- import bike blueprint from bike file
