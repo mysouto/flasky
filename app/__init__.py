1d6eee06ed83eba1dabf03ec77feaa709adf5e5f
Milena Souto <yamadasouto@gmail.com>
2022-11-01 11:05:35 -0700

    Add 2 configurations for different database environments development and testing

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,37 @@ from flask import Flask
 # can refer back from previous projects
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+# import for new env
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+# takes key value pairs in .env file: SQLALCHEMY_DATABASE_URI,
+# and make them available as environ variables thru os
+load_dotenv()
 
 
 # APP FUNCTION registers blueprint
 # function needs to be named exactly "create_app"
 # when `run Flask` it will look for create_app function
-def create_app():
+def create_app(testing=None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
     # CONFIG FOR DATABASE
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # where database is on the internet
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/bikes_development"
+
+    # ADDING LOGIC FOR 2 CONFIGURATIONS FOR DATABASES
+    if testing is None:
+        # where database is on the internet
+        # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/bikes_development"
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")  # points to development database
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")  # points to testing database
 
     from app.models.bike import Bike
     # CONNECT DATABASE TO APP
