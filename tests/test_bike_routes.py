4e969721f65730e4c0a4b994c854405ed35bc96e
Milena Souto <yamadasouto@gmail.com>
2022-11-02 11:49:23 -0700

    Create test for bike POST route

--- tests/test_bike_routes.py
+++ tests/test_bike_routes.py
@@ -18,6 +18,7 @@ def test_get_one_bike_with_empty_db_returns_404(client):
 
 
 # import client + fixture name
+# two_bikes -> to import fixture, setup database and run tests with it
 def test_get_one_bike_with_populated_db_returns_bike_json(client, two_bikes):
     response = client.get("/bike/1")
     response_body = response.get_json()
@@ -30,3 +31,19 @@ def test_get_one_bike_with_populated_db_returns_bike_json(client, two_bikes):
         "size": 30,
         "type": "hybrid"
     }
+
+
+# Create tests for POST requests
+def test_post_one_bike_creates_bike_in_db(client, two_bikes):
+    response = client.post("/bike", json={
+        "name": "New new",
+        "price": 80,
+        "size": 70,
+        "type": "blah"
+    })
+
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert "id" in response_body
+    assert response_body["id"] == 3
