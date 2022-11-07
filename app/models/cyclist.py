1a277b744b947b5de1e577b9ed2c003b686df510
Milena Souto <yamadasouto@gmail.com>
2022-11-07 11:59:31 -0800

    Add relationhip to Bike

--- app/models/cyclist.py
+++ app/models/cyclist.py
@@ -4,12 +4,21 @@ from app import db
 class Cyclist(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
+    # add relation to bikes
+    bikes = db.relationship("Bike", back_populates="cyclist")
+    # "cyclist" is the attribute defined in Bike model
+    # cyclist = db.relationship("Cyclist", back_populates="bikes")
 
     def to_dict(self):
+        bikes_list = [bike.to_dict() for bike in self.bikes]
+
         cyclist_dict = {
             "id": self.id,
             "name": self.name,
+            # add new key:value
+            "bikes": bikes_list,
         }
+
         return cyclist_dict
 
     @classmethod
